0297b5854a6b64fb8708917fc3a0c04c48499ba6
nishant-sr <nishant.srinivasan236@gmail.com>
2024-03-27 20:37:27 -0700

    "fixing code"

--- deleteAccount.php
+++ deleteAccount.php
@@ -1,8 +1,8 @@
 <?php
     include "config.php";
     session_start();
-    $id = $_SESSION['id'];
-    $sql = "DELETE FROM user WHERE user_id = $id";
+    $uid = $_SESSION['user_id'] ;
+    $sql = "DELETE FROM users WHERE user_id = $uid";
     $result = mysqli_query($conn, $sql);
 
     if ($result) {
@@ -13,5 +13,15 @@
     } else {
     echo "Failed: " . mysqli_error($conn);
     }
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the login page or any other page as needed
+header("Location: login.php"); // Redirect to login page
+exit;
 
 ?>
--- home.php
+++ home.php
@@ -24,9 +24,9 @@ if(isset($_SESSION['username'])&& isset($_SESSION['user_id'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-  <nav class="navbar bg-primary ">
+<nav class="navbar bg-primary ">
     <div class="col p-2">
-      <a href="index.html" class="link-light link-underline-opacity-25 link-underline-opacity-100-hover p-2">
+      <a href="index.php" class="link-light link-underline-opacity-25 link-underline-opacity-100-hover p-2">
         Main
       </a>
       <a href="timeline.php" class="link-light link-underline-opacity-25 link-underline-opacity-100-hover p-2">
@@ -50,6 +50,13 @@ if(isset($_SESSION['username'])&& isset($_SESSION['user_id'])) {
             <a href="create.php" class="link-light link-underline-opacity-25 link-underline-opacity-100-hover p-2">
                 Create
             </a>';
+        }
+
+        if($_SESSION['is_admin'] == 1){
+          echo"
+          <a href='admin.php' class='link-light link-underline-opacity-25 link-underline-opacity-100-hover p-2'>
+          Admin
+        </a>";
         }
     ?>
     </div>
